6edf4432804a0dea954791b05c93bacd81500e2b
Thinh <Thinh@gmail.com>
2023-09-20 16:13:45 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.routes");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +13,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
